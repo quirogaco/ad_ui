6779df1c78232fe3335381adca595c67073c3dcd
Quirogaco <quirogaco@gmail.com>
2023-04-19 22:39:49 -0400

    primer commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
 import './style.css'
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({})
+
+const app = createApp(App)
+app.use(vuetify);
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
-//import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 import VueMacros from 'unplugin-vue-macros/vite'
 import Vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
-      VueMacros({
-        plugins: {
-          vue: Vue(),
-          // vueJsx: VueJsx(), // if needed
-        },
-      }),
+        VueMacros({
+            plugins: {
+                vue: Vue()
+            },
+        }),
+
+        vuetify({
+            autoImport: true
+        })
     ],
   })
